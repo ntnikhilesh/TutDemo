2fe4fed4835d938dbe28f9f5be2707f977de9219
nikhil <ntnikhilesh@gmail.com>
2017-02-26 09:55:28 +0530

    adding more heroes in Array also handle their click events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
   title = 'Tour of heroes';
+//Initializing array object
 
-  //Creating object of clss Hero
-  hero:Hero={
- 
-  id:101,
-   name:"Ravi"
+heroes:Hero[]=HEROES;
+
+
+//Handle on click event of Hero list item
+selectedHero:Hero;
+  onSelect(hero:Hero):void {
+  this.selectedHero=hero;
   }
+
+
+//Creating object of class Hero
+ //hero:Hero={
+ 
+  //id:101,
+  // name:"Ravi"
+  //}; 
+
+
+  
+
+ 
 }
+
+
+
+
 // creating class 
-export class Hero{
-id:number;
+ export class Hero{
+	id:number;
 	name:string;
 	
 
-}
+}  
+
+//Crreating array of class object
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
